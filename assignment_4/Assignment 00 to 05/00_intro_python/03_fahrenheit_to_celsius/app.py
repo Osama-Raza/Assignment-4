e5eb6935b3c5d799915b5c0fd7f2bcafcd18cd44
Osama-Raza <138793946+Osama-Raza@users.noreply.github.com>
2025-04-06 20:07:03 +0500

    Delete assignment_4/Assignment 00 to 05/00_intro_python/03_fahrenheit_to_celsius/app.py
    d

--- assignment_4/Assignment 00 to 05/00_intro_python/03_fahrenheit_to_celsius/app.py
+++
@@ -1,5 +0,0 @@
-fahrenheit  = float (input('Enter a temperature in fahrenheit: '))
-print(f'Temperature: {fahrenheit}F')
-
-celsius = (fahrenheit - 32) * 5.0/9.0
-print(f'Temperature:{celsius: .2f}C')
